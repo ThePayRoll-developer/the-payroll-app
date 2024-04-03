de0a1972887408e27eb623d47e502b7b7b9ec134
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2024-04-03 13:06:13 +0600

    done

--- PayrollProjectNew/payroll_app/android/app/src/main/java/com/payroll/payroll_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.payroll.payroll_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- PayrollProjectNew/payroll_app/lib/src/routing/route.dart
+++
@@ -1,8 +0,0 @@
-class MyRoute{
-  static String bonusRoute="/bonus";
-  static String  advanceRoute="/advance";
-  static String  leaveRoute="/leave";
-  // static String  aRoute="/task";
-  // static String  taskFormRoute="/task_form";
-  // static String  timeActionRoute="/time_action";
-}
